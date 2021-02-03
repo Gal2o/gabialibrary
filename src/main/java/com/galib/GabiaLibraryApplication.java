9d89c3cf0e9c0f1b50181f3bba36df58fa8a9c6f
didrlgus <rlgusdid@naver.com>
2021-02-03 14:28:48 +0900

    first commit

---
+++ src/main/java/com/galib/GabiaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.galib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabiaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabiaLibraryApplication.class, args);
+    }
+
+}
