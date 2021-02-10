f6cbaa91599b69f890568d5b5ebdde6c08f9dea7
didrlgus <rlgusdid@naver.com>
2021-02-10 09:48:01 +0900

    ADD/zuul dependency 추가 및 설정 완료

--- spring-cloud/api-gateway/src/main/java/gabia/library/ApiGatewayApplication.java
+++ spring-cloud/api-gateway/src/main/java/gabia/library/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package gabia.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
