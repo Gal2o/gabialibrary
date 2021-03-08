966f8b5942945fcc44711e91c5af46f3c9801c7e
Wade(양기현) <ygh@gabia.com>
2021-03-08 11:32:19 +0900

    ADD/eureka server 생성 및 통합세팅
    * eureka를 이용한 discovery 서버 생성 및 세팅
    * eureka client 세팅
    * follow-up issue로 남겨두었던 URL 하드코딩 부분 dynamic하게 변경

--- spring-cloud/api-gateway/src/main/java/gabia/library/ApiGatewayApplication.java
+++ spring-cloud/api-gateway/src/main/java/gabia/library/ApiGatewayApplication.java
@@ -2,11 +2,11 @@ package gabia.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ApiGatewayApplication {
