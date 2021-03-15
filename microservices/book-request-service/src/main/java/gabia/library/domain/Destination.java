a1770878ffefac93cc0f2319404a99f4a06cc463
최현민 <chm@gabia.com>
2021-03-15 20:52:20 +0900

    FIX/book-request-service
    ENUM 수정

--- microservices/book-request-service/src/main/java/gabia/library/domain/Destination.java
+++ microservices/book-request-service/src/main/java/gabia/library/domain/Destination.java
@@ -1,15 +1,14 @@
 package gabia.library.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum Destination {
-    Headquarter("본사"),
+    HEAD("본사"),
     SEOCHO("서초IDC"),
     GASAN("가산IDC"),
     WCENTER("가산W센터");
 
-    private final String destination;
+    Destination(String value) {
+        this.value = value;
+    }
+    private final String value;
+    public String value() { return value; }
 }
--- microservices/book-request-service/src/main/java/gabia/library/domain/Status.java
+++ microservices/book-request-service/src/main/java/gabia/library/domain/Status.java
@@ -1,15 +1,12 @@
 package gabia.library.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum Status {
     REQUESTED("요청중"),
     CANCELED("취소"),
     BUYING("구매중"),
     COMPLETED("구매완료");
 
-    private final String status;
+    Status(String value) { this.value = value; }
+    private final String value;
+    public String value() { return value; }
 }
