4859ade827fc99be3cffcdc2abc2bd0d239b2b6e
didrlgus <rlgusdid@naver.com>
2021-03-16 18:17:18 +0900

    UPDATE/도서신청 시 알람 메일 전송 로직 업데이트

--- microservices/book-request-service/src/main/java/gabia/library/dto/UserBookRequestDto.java
+++ microservices/book-request-service/src/main/java/gabia/library/dto/UserBookRequestDto.java
@@ -1,8 +1,6 @@
 package gabia.library.dto;
 
-import gabia.library.domain.Destination;
 import gabia.library.domain.Status;
-import gabia.library.exception.BusinessException;
 import gabia.library.kafka.BookRequestMessage;
 import gabia.library.utils.alert.AlertType;
 import lombok.AllArgsConstructor;
@@ -26,27 +24,10 @@ public class UserBookRequestDto {
                 .email(email)
                 .bookTitle(bookRequestDto.getBookName())
                 .bookAuthor(bookRequestDto.getAuthor())
-                .destination(getDestinationValue(bookRequestDto.getDestination().value()))
+                .destination(bookRequestDto.getDestination().value())
                 .requestState(Status.REQUESTED.value())
                 .alertType(AlertType.REQUESTED)
                 .build();
     }
 
-    private String getDestinationValue(String destinationVal) {
-        String destination = "";
-
-        if (destinationVal.equals("HEAD")) {
-            destination = "본사";
-        } else if (destinationVal.equals("SEOCHO")) {
-            destination = "서초IDC";
-        } else if (destinationVal.equals("GASAN")) {
-            destination = "가산IDC";
-        } else if (destinationVal.equals("WCENTER")) {
-            destination = "가산W센터";
-        } else {
-            throw new BusinessException("올바르지 않은 목적지 입니다.");
-        }
-
-        return destination;
-    }
 }
